df33bf048baf08fb318c99c9ffef282f7a8b1649
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-03 13:36:14 +0300

    feat: add new SVG icons and implement ServiceCard component in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import About from "@/components/about";
 import Hero from "@/components/hero";
+import Partners from "@/components/partners";
+import Services from "@/components/services";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Partners />
+      <About />
+      <Services />
     </main>
   );
 }
---
+++ src/components/serviceCard.tsx
@@ -0,0 +1,24 @@
+import Image, { StaticImageData } from 'next/image'
+import React from 'react'
+
+type ServiceCardProps = {
+    src: StaticImageData,
+    title: string,
+    subTitle: string,
+}
+
+export default function ServiceCard({ src, title, subTitle }: ServiceCardProps) {
+    return (
+        <div className='flex flex-col justify-start items-center max-w-sm'>
+            <div className='mb-8 size-[152px] bg-white rounded-md p-1'>
+                <div className='bg-dark-purple rounded-full w-full h-full flex justify-center items-center'>
+                    <Image src={src} alt={title} width={64} height={64} className=' ' />
+                </div>
+            </div>
+            <div className="max-w-xs text-center">
+                <h3 className='mb-2.5'>{title}</h3>
+                <p className='text-neutral-500'>{subTitle}</p>
+            </div>
+        </div>
+    )
+}
