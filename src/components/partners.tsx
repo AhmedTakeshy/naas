7508070bd17716306cae43dd4e56cb3bb101ba12
Ahmed Takeshy <Ahmedtakeshy7@gmail.com>
2025-01-03 13:35:34 +0300

    feat: add InfiniteMovingCards component and Partners section with animations

---
+++ src/components/partners.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { InfiniteMovingCards } from './infinite-moving-cards'
+import cibIco from "@/../public/icons/cib.svg";
+import bmwIco from "@/../public/icons/bmw.svg";
+import huaweiIco from "@/../public/icons/huawei.svg";
+import nasaIco from "@/../public/icons/nasa.svg";
+import { StaticImageData } from 'next/image';
+
+export default function Partners() {
+    const items: { src: StaticImageData, alt: string }[] = [
+        {
+            src: cibIco,
+            alt: "cib bank image",
+        },
+        {
+            src: bmwIco,
+            alt: "bmw logo image",
+        },
+        {
+            src: huaweiIco,
+            alt: "huawei logo image",
+        },
+        {
+            src: nasaIco,
+            alt: "nasa logo image",
+        },
+    ];
+    return (
+        <section className="h-[40rem] rounded-md flex flex-col antialiased sm:-mt-8 bg-white dark:bg-darker  items-center justify-center relative overflow-hidden">
+            <h2 className='mb-16'>
+                Our Partners
+            </h2>
+            <InfiniteMovingCards
+                items={items}
+                direction="right"
+                speed="slow"
+            />
+        </section>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,8 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+import colors from "tailwindcss/colors";
+import { default as flattenColorPalette } from "tailwindcss/lib/util/flattenColorPalette";
+
 import type { Config } from "tailwindcss";
 
 export default {
@@ -68,8 +73,30 @@ export default {
 					xl: '4rem',
 					"2xl": '5rem'
 				}
-			}
-		}
+			},
+			animation: {
+				scroll:
+					"scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+			},
+			keyframes: {
+				scroll: {
+					to: {
+						transform: "translate(calc(-50% - 0.5rem))",
+					},
+				},
+			},
+		},
 	},
-	plugins: [require("tailwindcss-animate")],
+	plugins: [require("tailwindcss-animate"), addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
